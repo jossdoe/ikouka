dc15a649a202344c1bf4cbc5d8b829b0bc9451dc
Joss Doebler <joss@neuenarrative.de>
2023-06-25 14:01:44 +0200

    add tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,25 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        postCss: true,
+      },
+    },
+  ],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
-    check: true // type-check stories during Storybook build
+    check: true, // type-check stories during Storybook build
   },
 
   framework: {
     name: '@storybook/react-webpack5',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
