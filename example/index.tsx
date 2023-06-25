e9c57d1d1b527516252577dcfcb864c42ce21da4
Joss Doebler <joss@neuenarrative.de>
2023-06-25 14:18:31 +0200

    setup examples and update actions

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,8 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Thing } from 'ikouka';
+import 'ikouka/dist/tailwind.css';
 
 const App = () => {
   return (
