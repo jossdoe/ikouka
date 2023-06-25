840261338d349239dbd3d2d13bea86040476526b
Joss Doebler <joss@neuenarrative.de>
2023-06-25 18:43:21 +0200

    add Button-component

---
+++ src/components/Button.tsx
@@ -0,0 +1,46 @@
+import React, { FC, HTMLAttributes } from 'react';
+import cn from 'classnames';
+
+export type ButtonProps = {
+  children: string;
+  variant: 'primary' | 'secondary' | 'text';
+  href?: string;
+} & HTMLAttributes<HTMLButtonElement | HTMLAnchorElement>;
+
+/**
+ * A button component.
+ */
+export const Button: FC<ButtonProps> = ({
+  children,
+  className,
+  variant,
+  href,
+  ...rest
+}) => {
+  const css = cn({
+    'px-4 py-2 rounded': true,
+    [className || '']: true,
+    'bg-blue-100 text-blue-600 hover:bg-blue-200': variant === 'primary',
+    'bg-gray-100 text-gray-600 hover:bg-gray-200': variant === 'secondary',
+    'bg-transparent text-gray-600 hover:bg-gray-100': variant === 'text',
+  });
+
+  if (href)
+    return (
+      <a
+        className={css}
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        {...rest}
+      >
+        {children}
+      </a>
+    );
+
+  return (
+    <button className={css} {...rest}>
+      {children}
+    </button>
+  );
+};
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Thing, Props } from '../src';
+import { Button, ButtonProps } from '../src';
 
 const meta: Meta = {
-  title: 'Welcome',
-  component: Thing,
+  title: 'Button',
+  component: Button,
   argTypes: {
     children: {
       control: {
@@ -15,11 +15,14 @@ const meta: Meta = {
   parameters: {
     controls: { expanded: true },
   },
+  args: {
+    children: 'Click Me',
+  },
 };
 
 export default meta;
 
-const Template: Story<Props> = args => <Thing {...args} />;
+const Template: Story<ButtonProps> = args => <Button {...args} />;
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
