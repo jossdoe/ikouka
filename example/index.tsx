48672de573e0731ef609f212f1ccd689d157bd22
Joss Doebler <joss@neuenarrative.de>
2023-07-01 22:26:14 +0200

    chore: update example

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,16 @@
 import 'react-app-polyfill/ie11'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { Thing } from 'ikouka'
+import { Badge, Button } from 'ikouka'
 import 'ikouka/dist/tailwind.css'
 
 const App = () => {
   return (
-    <div>
-      <Thing />
+    <div
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <Badge color="purple">Badge</Badge>
+      <Button size="sm">Click me</Button>
     </div>
   )
 }
