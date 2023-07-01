40f8f0149dd00817b463887014c1dae57dbc262c
Joss Doebler <joss@neuenarrative.de>
2023-07-01 21:06:36 +0200

    refactor: use CSF 3.0 for stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,31 +1,27 @@
-import React from 'react';
-import { Meta, Story } from '@storybook/react';
-import { Button, ButtonProps } from './index';
+import { Button } from './index';
 
-const meta: Meta = {
+export default {
   title: 'Button',
   component: Button,
-  argTypes: {
-    children: {
-      control: {
-        type: 'text',
-      },
-    },
-  },
-  parameters: {
-    controls: { expanded: true },
-  },
+};
+
+export const Primary = {
   args: {
     children: 'Click Me',
+    variant: 'primary',
   },
 };
 
-export default meta;
+export const Secondary = {
+  args: {
+    children: 'Click Me',
+    variant: 'secondary',
+  },
+};
 
-const Template: Story<ButtonProps> = args => <Button {...args} />;
-
-// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
-// https://storybook.js.org/docs/react/workflows/unit-testing
-export const Default = Template.bind({});
-
-Default.args = {};
+export const Text = {
+  args: {
+    children: 'Click Me',
+    variant: 'text',
+  },
+};
--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Button } from './Button.stories';
+import { Button } from './index';
 
 describe('Button', () => {
   it('renders without crashing', () => {
