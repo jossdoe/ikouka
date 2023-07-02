ba05b7c788c95c94ce6602a9a9720d200923901c
Joss Doebler <joss@neuenarrative.de>
2023-07-02 12:40:44 +0200

    test: replace direct rtl-imports with a proxy-file

--- src/components/Badge/Badge.test.tsx
+++ src/components/Badge/Badge.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '../../utils/testing'
 import { Badge, BadgeProps } from './index'
 
 const LABEL_TEXT = 'Badge Text'
---
+++ src/utils/testing.ts
@@ -0,0 +1,4 @@
+// This is a proxy-file to allow for global wrappers and modifications
+// to the functionality of React Testing Library.
+// https://testing-library.com/docs/react-testing-library/setup#custom-render
+export * from '@testing-library/react'
