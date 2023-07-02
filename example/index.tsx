eab1a4193b6aa67a5a7bd03257f2c80f92e7fdca
Joss Doebler <joss@neuenarrative.de>
2023-07-02 17:23:50 +0200

    docs: update example

--- example/index.tsx
+++ example/index.tsx
@@ -1,18 +1,26 @@
 import 'react-app-polyfill/ie11'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { Badge, Button, Stack } from 'ikouka'
+import { Badge, Button, Stack, Wrap } from 'ikouka'
 import 'ikouka/dist/tailwind.css'
 
 const App = () => {
   return (
-    <Stack size="sm">
+    <Stack space="lg">
       <div>
         <Badge color="purple">Badge</Badge>
       </div>
       <div>
         <Button size="sm">Click me</Button>
       </div>
+      <Wrap space="lg">
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => (
+          <div
+            key={value}
+            style={{ height: '58px', width: '100px', background: 'red' }}
+          />
+        ))}
+      </Wrap>
     </Stack>
   )
 }
