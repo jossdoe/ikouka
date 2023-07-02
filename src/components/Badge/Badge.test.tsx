af7b34d32df40e3fa02904822258c41a32f68dfc
Joss Doebler <joss@neuenarrative.de>
2023-07-02 12:35:35 +0200

    test: write tests for Badge-component

---
+++ src/components/Badge/Badge.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { Badge, BadgeProps } from './index'
+
+const LABEL_TEXT = 'Badge Text'
+
+const COLORS: BadgeProps['color'][] = [
+  'gray',
+  'green',
+  'red',
+  'purple',
+  undefined
+]
+
+const VARIANTS: BadgeProps['variant'][] = ['filled', 'outline', undefined]
+
+const SIZES: BadgeProps['size'][] = ['md', 'lg', undefined]
+
+COLORS.forEach((color) => {
+  VARIANTS.forEach((variant) => {
+    SIZES.forEach((size) => {
+      test(`Badge – ${color}, ${variant}, ${size}`, async () => {
+        render(
+          <Badge color={color} variant={variant} size={size}>
+            {LABEL_TEXT}
+          </Badge>
+        )
+
+        expect(screen.getByText(LABEL_TEXT)).toBeInTheDocument()
+      })
+    })
+  })
+})
