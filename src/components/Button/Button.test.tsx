d2d6d7cd577f55bc9c646ed715aeadb0d09d6e8f
Joss Doebler <joss@neuenarrative.de>
2023-07-02 12:30:29 +0200

    test: write tests for Button-component

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,11 +1,56 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import { Button } from './index'
+import userEvent from '@testing-library/user-event'
+import { Button, ButtonProps } from './index'
 
-describe('Button', () => {
-  it('renders without crashing', () => {
-    render(<Button variant="primary">Click Me</Button>)
-    const button = screen.getByText(/click me/i)
-    expect(button).toBeInTheDocument()
+const LABEL_TEXT = 'Click Me'
+
+const VARIANTS: ButtonProps['variant'][] = [
+  'primary',
+  'secondary',
+  'text',
+  undefined
+]
+
+const SIZES: ButtonProps['size'][] = ['sm', 'md', 'lg', undefined]
+
+VARIANTS.forEach((variant) => {
+  SIZES.forEach((size) => {
+    test(`Button (onClick) – ${variant}, ${size}`, async () => {
+      const onClick = jest.fn()
+
+      render(
+        <Button variant={variant} size={size} onClick={onClick}>
+          {LABEL_TEXT}
+        </Button>
+      )
+
+      expect(screen.getByRole('button')).toHaveTextContent(LABEL_TEXT)
+
+      expect(onClick.mock.calls).toHaveLength(0)
+      await userEvent.click(screen.getByText(LABEL_TEXT))
+      expect(onClick.mock.calls).toHaveLength(1)
+    })
+  })
+})
+
+VARIANTS.forEach((variant) => {
+  SIZES.forEach((size) => {
+    test(`Button (href) – ${variant}, ${size}`, async () => {
+      render(
+        <Button variant={variant} size={size} href="#">
+          {LABEL_TEXT}
+        </Button>
+      )
+
+      expect(screen.getByRole('link')).toHaveTextContent(LABEL_TEXT)
+      expect(screen.getByRole('link')).toHaveAccessibleName(LABEL_TEXT)
+      expect(screen.getByRole('link')).toHaveAttribute('href', '#')
+      expect(screen.getByRole('link')).toHaveAttribute('target', '_blank')
+      expect(screen.getByRole('link')).toHaveAttribute(
+        'rel',
+        'noopener noreferrer'
+      )
+    })
   })
 })
