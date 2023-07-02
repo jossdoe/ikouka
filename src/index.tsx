0034d161be9d249740a4ad3611a1b556fed168e1
Joss Doebler <joss@neuenarrative.de>
2023-07-02 16:57:23 +0200

    fix: export Stack from lib

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,3 @@
-export { Button, ButtonProps } from './components/Button'
 export { Badge, BadgeProps } from './components/Badge'
+export { Button, ButtonProps } from './components/Button'
+export { Stack, StackProps } from './components/Stack'
